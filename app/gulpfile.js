85650d13ae75255406444620f3ef3bad94f9d54e
Jack <jack@Zoostormingitup.(none)>
2015-07-18 15:52:37 +0100

    Developed project tree :cyclone:

---
+++ app/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+  gulp.src('./temps/sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./static/css'));
+});
+
+gulp.task('sass:watch', function(){
+  gulp.watch('./temps/sass/*.scss', ['sass']);
+});
+
+gulp.default(['sass:watch']);
