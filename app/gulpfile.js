d29e431e29b5127c9ba6fc95b3d27b3d3f5abd95
chickencoder <jesse_sibley@outlook.com>
2015-07-20 13:23:24 +0100

    Included nodemon into gulpfile :innocent:

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,6 +1,7 @@
 // gulpfile.js
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var node = require('gulp-nodemon');
 
 gulp.task('sass', function(){
   gulp.src('./temps/sass/*.scss')
@@ -8,8 +9,16 @@ gulp.task('sass', function(){
     .pipe(gulp.dest('./static/css'));
 });
 
+gulp.task('start', function(){
+  node({
+    script: 'app.js',
+    ext: 'js html css',
+    watch: 'temps'
+  });
+});
+
 gulp.task('sass:watch', function(){
   gulp.watch('./temps/sass/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass:watch']);
+gulp.task('default', ['sass:watch', 'start']);
