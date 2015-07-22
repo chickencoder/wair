5d7a5d45dd8eb1f7e863219b9aa41ba2919a1591
Priceyy <jackprice@Jack-Prices-MacBook.local>
2015-07-22 18:11:41 +0100

    Working on backend :wrench:

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var swig    = require('swig');
+var fs      = require('fs');
 var app = express();
 
 // Swig Templates
@@ -14,7 +15,19 @@ app.set('view cache', false);
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(req, res){
-  res.render('index', {});
+  var file;
+  fs.readFile(__dirname + '/eu.json', 'utf8', function(err, data){
+    if (err) {
+      console.log(err);
+    }
+    file = data.toString();
+  });
+
+  var airports = JSON.parse(file);
+  for (var a in airports) {
+    console.log(airports[a]);
+  }
+  res.render('index', {'airports': {}});
 });
 
 app.get('/results', function(req, res){
@@ -26,5 +39,5 @@ app.get('/test', function(req, res){
   res.render('test', {});
 });
 
-app.listen(6000);
-console.log("Listening on port 6000...");
+app.listen(9000);
+console.log("Listening on port 9000...");
